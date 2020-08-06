eb7be435b5462d16505c41096a16a540e4a4da6a
Damien Zufferey <zufferey@mpi-sws.org>
2020-08-06 19:52:41 +0200

    small fixies

--- findId.sh
+++ findId.sh
@@ -4,11 +4,11 @@ declare -A ids
 ids=( ["REPLICA0"]="0"
       ["REPLICA1"]="1"
       ["REPLICA2"]="2"
-      ["REPLICA3"]="3" 
-      ["REPLICA4"]="4" 
-      ["REPLICA5"]="5" 
-      ["REPLICA6"]="6" 
+      ["REPLICA3"]="3"
+      ["REPLICA4"]="4"
+      ["REPLICA5"]="5"
+      ["REPLICA6"]="6"
       ["REPLICA7"]="7"
-      ["REPLICA8"]="8" ) #XXX put the name of the machine on which the system will run
+      ["REPLICA8"]="8" )
 
 echo ${ids[`hostname`]}
