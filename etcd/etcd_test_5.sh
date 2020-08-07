2371f0a9f26f66cf398b4b6acfc7034f66de5c16
Damien Zufferey <zufferey@mpi-sws.org>
2020-08-07 10:22:32 +0200

    update etcd script

--- etcd/etcd_test_5.sh
+++ etcd/etcd_test_5.sh
@@ -2,15 +2,17 @@
 
 rm -rf ~/etcd_storage/data
 
-id=$((`$RESYNC/findId.sh` + 1))
+id=`$RESYNC/findId.sh`
+ips=(REPLICA0 REPLICA1 REPLICA2 REPLICA3 REPLICA4 REPLICA5 REPLICA6 REPLICA7 REPLICA8)
+ip=${ips[$id]}
 
 exec $ETCD/bin/etcd --name s$id \
 	--data-dir ~/etcd_storage/data \
-	--listen-client-urls http://REPLICA0:2379 \
-	--advertise-client-urls http://REPLICA0:2379 \
-	--listen-peer-urls http://REPLICA0:2380 \
-	--initial-advertise-peer-urls http://REPLICA0:2380 \
-	--initial-cluster s1=http://REPLICA0:2380,s2=http://REPLICA1:2380,s3=http://REPLICA2:2380,s4=http://REPLICA3:2380,s5=http://REPLICA4:2380 \
+	--listen-client-urls http://$ip:2379 \
+	--advertise-client-urls http://$ip:2379 \
+	--listen-peer-urls http://$ip:2380 \
+	--initial-advertise-peer-urls http://$ip:2380 \
+	--initial-cluster s0=http://REPLICA0:2380,s1=http://REPLICA1:2380,s2=http://REPLICA2:2380,s3=http://REPLICA3:2380,s4=http://REPLICA4:2380 \
 	--initial-cluster-token tkn \
 	--initial-cluster-state new \
 	--heartbeat-interval '1000' \
